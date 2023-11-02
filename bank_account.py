e79be99b60837b730201f18daa7ec7690cc681ff
Thunyanan Tangpipatpong <thunynan.t@ku.th>
2023-11-02 10:52:50 +0700

    Modify into OOP without delete function and example test case

--- bank_account.py
+++ bank_account.py
@@ -36,42 +36,20 @@ class Account:
         self.balance = balance
 
     def deposit(self, amount):
-        index = AccountDB.search_private(self.account_number)
-        if index != -1:
-            if self.balance >= amount:
-                print("Withdrawing", amount, "from", self.account_number)
-                self.balance -= amount
-            else:
-                print("withdrawal amount", amount, "exceeds the balance of", self.balance,
-                      "for", self.account_number, "account.")
-        else:
-            print(self.account_number, "invalid account number; no withdrawal action performed.")
-            self.balance += amount
+        print("Depositing", amount, "to", self.account_number)
+        self.balance += amount
 
     def withdraw(self, amount):
         if self.balance >= amount:
+            print("Withdrawing", amount, "from", self.account_number)
             self.balance -= amount
+        else:
+            print("withdrawal amount", amount, "exceeds the balance of", self.balance,
+                  "for", self.account_number, "account.")
 
     def __str__(self):
         return '{' + str(self.account_number) + ',' + str(self.type) + ',' + \
                str(self.account_name) + ',' + str(self.balance) + '}'
 
 
-account1 = Account("0000", "saving", "David Patterson", 1000)
-account2 = Account("0001", "checking", "John Hennessy", 2000)
-account3 = Account("0003", "saving", "Mark Hill", 3000)
-account4 = Account("0004", "saving", "David Wood", 4000)
-account5 = Account("0004", "saving", "David Wood", 4000)
-my_account_DB = AccountDB()
-my_account_DB.insert(account1)
-my_account_DB.insert(account2)
-my_account_DB.insert(account3)
-my_account_DB.insert(account4)
-my_account_DB.insert(account5)
-print(my_account_DB)
-my_account_DB.search_public("0003").deposit(50)
-print(my_account_DB)
-my_account_DB.search_public("0003").withdraw(100)
-print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
-print(my_account_DB)
+
