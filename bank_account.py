3738232e07beb9218070cfe36d7891c648e795c1
Thunyanan Tangpipatpong <thunynan.t@ku.th>
2023-11-02 10:58:35 +0700

    Finising 1st task

--- bank_account.py
+++ bank_account.py
@@ -3,13 +3,13 @@ class AccountDB:
         self.account_database = []
 
     def insert(self, account):
-        index = self.search_private(account.account_number)
+        index = self.__search_private(account.account_number)
         if index == -1:
             self.account_database.append(account)
         else:
             print("Account", account.account_number, "already exists")
 
-    def search_private(self, account_num):
+    def __search_private(self, account_num):
         for i in range(len(self.account_database)):
             if self.account_database[i].account_number == account_num:
                 return i
@@ -20,6 +20,14 @@ class AccountDB:
             if account.account_number == account_num:
                 return account
         return None
+
+    def delete_account(self, account_num):
+        index = self.search_public(account_num)
+        if index != -1:
+            print("Deleting account:", account_num)
+            self.account_database.remove(account_num)
+        else:
+            print(account_num, "invalid account number; nothing to be deleted.")
 
     def __str__(self):
         s = ''
@@ -52,4 +60,21 @@ class Account:
                str(self.account_name) + ',' + str(self.balance) + '}'
 
 
-
+account1 = Account("0000", "saving", "David Patterson", 1000)
+account2 = Account("0001", "checking", "John Hennessy", 2000)
+account3 = Account("0003", "saving", "Mark Hill", 3000)
+account4 = Account("0004", "saving", "David Wood", 4000)
+account5 = Account("0004", "saving", "David Wood", 4000)
+my_account_DB = AccountDB()
+my_account_DB.insert(account1)
+my_account_DB.insert(account2)
+my_account_DB.insert(account3)
+my_account_DB.insert(account4)
+my_account_DB.insert(account5)
+print(my_account_DB)
+my_account_DB.search_public("0003").deposit(50)
+print(my_account_DB)
+my_account_DB.search_public("0003").withdraw(100)
+print(my_account_DB)
+my_account_DB.search_public("0010").deposit(50)
+print(my_account_DB)
